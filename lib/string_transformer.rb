54373659f31eba9f9e86ea93ab85c62413ae2000
corasan <henrypl360@gmail.com>
2016-04-10 22:55:19 -0400

    removed the symbols from the encryption_string array, encrypt method now uses the first 5 letters and encrypted string ends with _$

--- lib/string_transformer.rb
+++ lib/string_transformer.rb
@@ -7,14 +7,14 @@ module StringTransformer
 
   # The method encryption_arr returns an array of characters that will replace each letter in a given string
   def encryption_arr
-    encryption_string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*+><?1234567890"
+    encryption_string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
     encryption_string.split("")
   end
 
   # The method encrypt replaces every character in a given string with the first 4 elements in encryption_arr
   def encrypt
     @@original_str = self
-    @@encrypt = @@original_str.split("").map { |letter| letter.replace(encryption_arr.shuffle.first(4).join) }.join
+    @@encrypt = @@original_str.split("").map { |letter| letter.replace(encryption_arr.shuffle.first(5).join) }.join + "_$"
     @@key = encryption_key(@@encrypt, @@original_str)
     @@encrypt
   end
--- lib/string_transformer/version.rb
+++ lib/string_transformer/version.rb
@@ -1,3 +1,3 @@
 module StringTransformer
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
 end
